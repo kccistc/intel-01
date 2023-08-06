a658a6a404dfa34dcc0171816c7311b25a723df7
litcoder <litcoderwiz@gmail.com>
2023-08-06 16:06:16 +0900

    Update multiprocess note book
    Signed-off-by: litcoder <litcoderwiz@gmail.com>

--- Class02/python-parallelism/subprocess_practice.py
+++
@@ -1,8 +0,0 @@
-import time
-import subprocess
-import os
-import psutil
-
-print("Subprocess running ... PID:{}, PPID:{}".format(os.getpid(), psutil.Process(os.getpid()).ppid()), flush=True)
-time.sleep(1)
-print("Subprocess done")
