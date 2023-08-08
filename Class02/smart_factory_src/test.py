9a6635e5c4d8284dc2c5a0b4fedd9ab78c5c1777
jun-ho-Kim <wnsgh5049@naver.com>
2023-08-08 19:46:20 +0900

    23.08.08.(Tue) Homework_junhoKim

---
+++ Class02/smart_factory_src/test.py
@@ -0,0 +1,27 @@
+from iotdemo import FactoryController
+import time
+
+with FactoryController('/dev/ttyACM0') as ctrl:
+    ctrl.red = False
+    ctrl.green = False
+    ctrl.orange = False
+    ctrl.conveyor = False
+    print("start")
+    time.sleep(1)
+    ctrl.system_start()
+    ctrl.system_stop()
+    time.sleep(1)
+    ctrl.red = True
+    time.sleep(1)
+    ctrl.green = True
+    time.sleep(1)
+    ctrl.orange = True
+    time.sleep(1)
+    ctrl.conveyor = True
+    time.sleep(1)
+    ctrl.push_actuator(1)
+    time.sleep(1)
+    ctrl.push_actuator(2)
+
+    print("end")
+    ctrl.close()
