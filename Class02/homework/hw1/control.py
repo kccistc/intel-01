abe8e71323aedd5912463b09470acf7b2c630010
JHaeYoung <godud7792@gmail.com>
2023-08-09 09:22:38 +0900

    정해영 homework2
    수정해서 다시 올립니다

--- Class02/homework/hw1/control.py
+++ Class02/homework/hw1/control.py
@@ -5,41 +5,41 @@ import time
 with FactoryController('/dev/ttyACM0') as ctrl:
     ctrl.system_start()
     time.sleep(1)
-    input = ''
-    while input !='q':
-        input = input()
-        if input == 1:
+    input_q = ''
+    while input_q !='q':
+        input_q = input()
+        if input_q == '1':
             ctrl.red=FactoryController.DEV_ON
             print("Done red.")
             time.sleep(1)        
             ctrl.red=FactoryController.DEV_OFF
             time.sleep(1)
-        if input == 2:
+        if input_q == '2':
             ctrl.orange=FactoryController.DEV_ON
             print("Done. orange")
             time.sleep(1)
             ctrl.orange=FactoryController.DEV_OFF
             time.sleep(1)
-        if input == 3:
+        if input_q == '3':
             ctrl.green=FactoryController.DEV_ON
             print("Done. green")
             time.sleep(1)
             ctrl.green=FactoryController.DEV_OFF
             time.sleep(1)
 
-        if input == 4:
+        if input_q == '4':
             ctrl.conveyor=FactoryController.DEV_ON
             print("Done. conveyor")
             time.sleep(1)
             ctrl.conveyor=FactoryController.DEV_OFF
             time.sleep(1)
         
-        if input == 5:
+        if input_q == '5':
             ctrl.push_actuator(1)
             print("Done. push_actuator1")
             time.sleep(1)            
 
-        if input == 6:
+        if input_q == '6':
             ctrl.push_actuator(2)
             print("Done. push_actuator2")
             time.sleep(1)
