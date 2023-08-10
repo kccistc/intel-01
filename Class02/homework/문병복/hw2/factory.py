d2e22af368ee41edf872e0e8c400184cfa675714
MBB <moonland2820@gmail.com>
2023-08-10 09:10:03 +0900

    다시 제출합니다

--- Class02/homework/문병복/hw2/factory.py
+++ Class02/homework/문병복/hw2/factory.py
@@ -19,8 +19,6 @@ def thread_cam1(q):
     # TODO: MotionDetector
 
     # TODO: Load and initialize OpenVINO
-    # TODO: HW2 Open video clip resources/factory/conveyor.mp4 instead of camera device.
-    # 프레임당 정보 얻기 (opencv 사용)
     cap = cv2.VideoCapture("./resources/factory/conveyor.mp4")
 
     while not FORCE_STOP:
@@ -29,9 +27,6 @@ def thread_cam1(q):
         if frame is None:
             break
 
-        # TODO: HW2 Enqueue "VIDEO:Cam1 live", frame info
-        # q.put({"VIDEO:Cam1 live": frame}) X
-        # imshow("VIDEO:Cam1 live", frame)
         q.put({"VIDEO:Cam1 live": frame})
         # TODO: Motion detect
 
@@ -57,10 +52,11 @@ def thread_cam1(q):
 
 
 def thread_cam2(q):
-    # TODO: MotionDetector
+    # TODO: Motion Detector
 
     # TODO: ColorDetector
-    # TODO: HW2 Open "resources/factory/conveyor.mp4" video clip
+    
+    
     cap = cv2.VideoCapture("./resources/factory/conveyor.mp4")
     while not FORCE_STOP:
         sleep(0.03)
@@ -68,9 +64,8 @@ def thread_cam2(q):
         if frame is None:
             break
 
-        # TODO: HW2 Enqueue "VIDEO:Cam2 live", frame info
-        # q.put({"VIDEO:Cam2 live": frame})
         q.put({"VIDEO:Cam2 live": frame})
+        
         # TODO: Detect motion
 
         # TODO: Enqueue "VIDEO:Cam1 detected", detected info.
@@ -107,10 +102,9 @@ def main():
                         help="Arduino port")
     args = parser.parse_args()
 
-    # TODO: HW2 Create a Queue
-    # data = q.모종의 방법() 처럼 생성
+
     q = Queue() 
-    # TODO: HW2 Create thread_cam1 and thread_cam2 threads and start them.
+    
     t1 = threading.Thread(target=thread_cam1, args=(q,))
     t2 = threading.Thread(target=thread_cam2, args=(q,))
 
@@ -123,9 +117,7 @@ def main():
             if cv2.waitKey(10) & 0xff == ord('q'):
                 break
             name =''
-            # TODO: HW2 get an item from the queue. You might need to properly handle exceptions.
-            # de-queue name and data
-            # q를 모종의 방법으로 까서 VIDEO:Cam1 live 인지 VIDEO:Cam2 live 인지 체크해서 해당 타이틀로 imshow
+            
             try:
                 item = q.get(timeout=1)  
                 if isinstance(item, dict):  
@@ -135,7 +127,6 @@ def main():
             except Empty:
                 continue  
 
-            # TODO: HW2 show videos with titles of 'Cam1 live' and 'Cam2 live' respectively.
             if name == 'VIDEO:Cam1 live' or name == 'VIDEO:Cam2 live':
                 imshow(name, data)
 
